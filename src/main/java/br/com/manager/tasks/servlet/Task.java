c3febd3de2e677a94fe4148ce7bb854045ec5bd8
LeviRegiani <levi.regiani@usp.br>
2022-03-15 17:23:08 -0300

    initial commit

---
+++ src/main/java/br/com/manager/tasks/servlet/Task.java
@@ -0,0 +1,14 @@
+package br.com.manager.tasks.servlet;
+
+import java.util.Date;
+
+public class Task {
+	private Integer id;
+	private String description;
+	private String Title;
+	private Date dateCreation;
+	private Date startDate;
+	private Date finalDate;
+	
+	
+}
